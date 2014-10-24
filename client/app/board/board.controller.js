3601352ce6c437b5b02f9b2a05176913978cefaf
Márcio Viegas <marcioaugustoviegas@gmail.com>
2014-10-24 18:49:31 -0200

    go rocket go

--- client/app/board/board.controller.js
+++ client/app/board/board.controller.js
@@ -13,7 +13,14 @@ angular.module('rocketBoardApp').controller('BoardController', function(Socket, 
     $scope.board = BoardManager.makeBoard(res.data);
   });
 
+  $scope.trigger = function(){
+    triggerRocketAnimation()
+  }
+
   $scope.assignMe = function(issue) {
+    if (issue.assignee && issue.assignee.id == $rootScope.user.id)
+      return;
+
     IssueManager.assignIssueToUser(issue, $rootScope.user);
     BoardManager.updateIssue(issue);
   };
@@ -26,11 +33,21 @@ angular.module('rocketBoardApp').controller('BoardController', function(Socket, 
   });
 
   $scope.boardSortOptions = {
+    containment: '#board',
     itemMoved: function(event) {
+
       var issue = event.source.itemScope.issue;
-      var column = event.dest.sortableScope.$parent.column;
-      IssueManager.changeStatus(issue, column.status);
-      BoardManager.updateIssue(issue, column.status);
+      var status;
+
+      if (event.dest.sortableScope.$id == 5) {
+        status = "5 - Done"
+        triggerRocketAnimation();
+      } else {
+        status = event.dest.sortableScope.$parent.column.status;
+      }
+
+      IssueManager.changeStatus(issue, status);
+      BoardManager.updateIssue(issue, status);
     },
     orderChanged: function() {
 
@@ -39,3 +56,36 @@ angular.module('rocketBoardApp').controller('BoardController', function(Socket, 
   };
 
 });
+
+var triggerRocketAnimation = function() {
+  $(".panel-done img.plain").hide();
+  $(".panel-done h3").css('opacity', 0);
+  $(".panel-done .issues-count").css('opacity', 0);
+  $(".panel-done img.colored").show().animate({
+    top: '-650px'
+  }, 2000, 'easeInBack', function() {
+    $(".panel-done img.colored").hide().css('top', 0);
+
+    $(".panel-done h3").text('Liftoff! We Have a Liftoff!');
+    $(".panel-done h3").css('color', '#5dc66c');
+    $(".panel-done h3").animate({
+      opacity: 1
+    }, 2000);
+
+    $(".panel-done .check-done").fadeIn(2000, function() {
+      $(".panel-done .check-done").hide();
+
+      $(".panel-done h3").css('opacity', 0);
+      $(".panel-done h3").text('Drop here to launch');
+      $(".panel-done h3").css('color', '#aaa');
+
+      $(".panel-done img.plain").fadeIn(600);
+      $(".panel-done h3").animate({
+        opacity: 1
+      }, 600);
+      $(".panel-done .issues-count").animate({
+        opacity: 1
+      }, 600);
+    });
+  });
+}
