52019c82ac76c8ae0925b99639eaebe34c1f8e8a
Márcio Viegas <marcioaugustoviegas@gmail.com>
2014-11-09 19:29:49 -0200

    change app name on grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function(grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'fullstackApp',
+        module: 'rocketBoardApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
